ff36c2658298efa3c6350c5ed8f7caeccf2425c0
Shreesha41223 <121144514+Shreesha41223@users.noreply.github.com>
2025-04-13 12:29:45 +0530

    removed uneccesary requirements

--- Assignment-10/URL_Shortner/URL_Shortner/settings.py
+++ Assignment-10/URL_Shortner/URL_Shortner/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,4 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
